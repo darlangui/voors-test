668292b296f0d835fee1e2c931a2d95e5cf18bee
Darlan Guimarães <darlaneduardoguimaraes@gmail.com>
2025-02-12 21:28:09 -0300

    chore(db): configured persistent storage using sails-disk adapter.

--- backend/config/env/development.js
+++ backend/config/env/development.js
@@ -3,8 +3,7 @@ module.exports = {
   environment: 'development',
   datastores: {
     default: {
-      adapter: 'sails-mysql',
-      url: 'mysql://tester:test@localhost:3306/teste_tecnico'
+      adapter: 'sails-disk'
     }
   }
 };
